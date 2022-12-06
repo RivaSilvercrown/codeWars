2a2bf2fa2167580b266ae6e0ff018071ff492e95
Riva <rivasilvercrown@gmail.com>
2022-12-06 18:32:37 +0100

    binary addition solution

---
+++ binary_add.py
@@ -0,0 +1,3 @@
+#Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+def add_binary(a,b):
+    return bin(a+b)[2:]
